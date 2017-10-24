cc18a16bfd686e90804e14e85283182b978fc148
mikazp <gnatenko.michaell@gmail.com>
2017-10-25 01:40:29 +0300

    add db settings to config

--- web/environments/dev/common/config/main-local.php
+++ web/environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=db;dbname=yii2-sample',
             'username' => 'root',
-            'password' => '',
+            'password' => 'secret',
             'charset' => 'utf8',
         ],
         'mailer' => [
